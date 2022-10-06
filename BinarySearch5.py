e79c866bb7ec70b890b49ade015899c5b43dc0e0
Марат <91172420+MaratSharshenaliev@users.noreply.github.com>
2022-10-06 14:51:03 +0600

    Update BinarySearch5.py

--- BinarySearch5.py
+++ BinarySearch5.py
@@ -9,3 +9,25 @@ class Solution:
             else:
                 end = mid - 1
         return start
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+            def binary_search(nums, target, FindMax):
+                keyIndex = -1
+                start, end = 0, len(nums)-1
+                while start <= end:
+                    middle = start + (end-start)//2
+                    if nums[middle] < target:
+                        start = middle + 1
+                    elif nums[middle] > target:
+                        end = middle - 1
+                    else:
+                        keyIndex = middle
+                        if FindMax:
+                            start = middle + 1
+                        else:
+                            end = middle - 1
+                return keyIndex
+            result = [-1, -1]
+            result[0] = binary_search(nums, target, False)
+            if result[0] != -1:
+                result[1] = binary_search(nums, target, True)
+            return result
