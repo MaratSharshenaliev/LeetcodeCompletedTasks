98aca29fc672fe737a3835ae151214360534ecab
Марат <91172420+MaratSharshenaliev@users.noreply.github.com>
2022-10-06 23:55:08 +0600

    Update BinarySearch6.py

--- BinarySearch6.py
+++ BinarySearch6.py
@@ -11,3 +11,14 @@ class Solution:
             else:
                 return mid
         return right
+    
+    
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+        l, r = 0, len(arr)
+        while l < r:
+            m = (l + r) // 2
+            if arr[m] - 1 - m < k:
+                l = m + 1
+            else:
+                r = m
+        return l + k
